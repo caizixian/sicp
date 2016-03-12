97a056a679bf81cda4c0aa0270a893fb0770b422
Ivan Cai <caizixian@users.noreply.github.com>
2016-03-12 21:37:00 +0800

    Exercise 1.20

---
+++ 1.2.6 expmod.py
@@ -0,0 +1,12 @@
+def remainder(a,b):
+    print("{}%{}".format(a,b))
+    return a%b
+
+def expmod(base,exp,m):
+    print("expmod({}, {}, {})".format(base,exp,m))
+    if exp==0:
+        return 1
+    elif exp%2==0:
+        return remainder(expmod(base,exp/2,m)**2, m)
+    else:
+        return remainder(expmod(base,exp-1,m)*base,m)
